0c6e2cdaa8d9381eb85b33c9670dcb929c274ca9
eightman <eightman124@gmail.com>
2025-10-05 14:21:40 +0900

    ?

--- Android/app/src/main/java/com/furinlab/eightman/uuid_gen/UuidGenApp.kt
+++
@@ -1,7 +0,0 @@
-package com.furinlab.eightman.uuid_gen
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class UuidGenApp : Application()
--- Android/app/src/main/java/com/furinlab/eightman/uuid_gen/ui/theme/Type.kt
+++
@@ -1,5 +0,0 @@
-package com.furinlab.eightman.uuid_gen.ui.theme
-
-import androidx.compose.material3.Typography
-
-val Typography = Typography()
