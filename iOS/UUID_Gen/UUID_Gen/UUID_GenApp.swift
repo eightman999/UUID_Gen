4baaa48b895d00238370d2459c282822f7511120
eightman <eightman124@gmail.com>
2025-10-05 12:33:12 +0900

    s

---
+++ iOS/UUID_Gen/UUID_Gen/UUID_GenApp.swift
@@ -0,0 +1,17 @@
+//
+//  UUID_GenApp.swift
+//  UUID_Gen
+//
+//  Created by eightman on 2025/10/05.
+//
+
+import SwiftUI
+
+@main
+struct UUID_GenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/UUID_Gen/UUID_GenTests/UUID_GenTests.swift
@@ -0,0 +1,17 @@
+//
+//  UUID_GenTests.swift
+//  UUID_GenTests
+//
+//  Created by eightman on 2025/10/05.
+//
+
+import Testing
+@testable import UUID_Gen
+
+struct UUID_GenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
